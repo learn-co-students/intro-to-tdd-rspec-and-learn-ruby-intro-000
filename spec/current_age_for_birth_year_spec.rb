cee414c2ba626d1d648dde4613d493ea8b372c32
weimin dong <dwmdwm2001910@gmail.com>
2017-01-22 03:14:17 +0000

    Done.

--- spec/current_age_for_birth_year_spec.rb
+++ spec/current_age_for_birth_year_spec.rb
@@ -3,7 +3,26 @@ require_relative '../current_age_for_birth_year.rb'
 describe "current_age_for_birth_year method" do
   it "returns the age of a person based on the year of birth" do
     age_of_person = current_age_for_birth_year(1984)
-    
-    expect(age_of_person).to eq(32)
+
+    expect(age_of_person).to eq(33)
+  end
+end
+
+describe "how_many_meal_in_months" do
+  it "returns how many meals in one month" do
+    meals_months = how_many_meal_in_months(5)
+
+    expect(meals_months).to eq(450)
+  end
+end
+
+describe "current_age_for_birth_year method" do
+  it "returns the age of a person based on the year of birth" do
+    current_year = Time.now.year
+    birth_year = 1984
+    answer = current_year - birth_year
+
+    age_of_person = current_age_for_birth_year(birth_year)
+    expect(age_of_person).to eq(answer)
   end
 end
