75d0d609934b8eeed3434446a82bb42b256b9069
zouhair khallaf <zouhairkhallaf@gmail.com>
2017-02-07 17:50:33 +0000

    Done.

--- how_old_are_you.rb
+++ how_old_are_you.rb
@@ -1,3 +1,3 @@
 def my_age_is (now_year, b_year)
-  return now_year - b_year
+  now_year - b_year
 end
--- spec/how_old_are_you_spec.rb
+++ spec/how_old_are_you_spec.rb
@@ -1,7 +1,7 @@
 require_relative '../how_old_are_you.rb'
 
-describe "how_old_are_you methode my_age_is() " do
-  it " calculates the age of the person based on the Birth year provided" do
+describe "FILE['how_old_are_you.rb'] Method['my_age_is(birth_year)']" do
+  it " Given birth year it calculates age" do
     my_age = my_age_is(2017, 1984)
     expect(my_age).to eq(33)
   end
