2e01b35afc506520af9f19d7b3cb912ed9a83d96
zouhair khallaf <zouhairkhallaf@gmail.com>
2017-02-07 17:44:26 +0000

    Done.

--- spec/how_old_are_you_spec.rb
+++ spec/how_old_are_you_spec.rb
@@ -1,7 +1,7 @@
 require_relative '../how_old_are_you.rb'
 
-describe "testing age generator" do
-  it "calculates the age of the person based on the Birth year provided" do
+describe "how_old_are_you methode my_age_is() " do
+  it " calculates the age of the person based on the Birth year provided" do
     my_age = my_age_is(2017, 1984)
     expect(my_age).to eq(33)
   end
