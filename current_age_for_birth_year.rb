56419a5d5edf38ebfcbd4cbd9a83b52c2a81e755
Keith Kerr <kkerr123@gmail.com>
2017-06-28 23:08:53 +0000

    Done.

---
+++ current_age_for_birth_year.rb
@@ -0,0 +1,6 @@
+def current_age_for_birth_year (birth_year)
+  #2003 is the "currenet year" this would be better as a library if current year was abstracted
+  #So this is saying subtract birth year from current year. Your argument is the little input that this method or machine takes in
+  #So elsewhere I can load this at the top and use puts current_age_for_birth_year(x) to get my answer
+  2003 - birth_year
+end
