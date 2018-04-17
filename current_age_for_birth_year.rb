8344abfc765df83647f8a94df9d5fc1d284494e7
DamBai212 <damanybailey@icloud.com>
2018-04-17 09:31:43 +0000

    Done.

--- current_age_for_birth_year.rb
+++ current_age_for_birth_year.rb
@@ -1,8 +1,1 @@
-def current_age_for_birth_year(birth_year)
-  2003 - birth_year
-end
-it "should return the current year for a person born in year 0" do
-  twenty_sixteen = current_age_for_birth_year(0)
-
-  expect(twenty_sixteen).to eq(2003)
-end
+def current_age_for_birth_year(birth_year) 2003 - birth_year end`
--- how_old_are_you.rb
+++ how_old_are_you.rb
@@ -1,5 +1,4 @@
 require_relative './current_age_for_birth_year.rb'
-
 puts "What year were you born?"
 birth_year = gets.to_i
 
