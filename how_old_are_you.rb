aa23cd6e1f29d8a3cee3ae6c8e389efb9b7edeb5
Olivia R <orquigley@gmail.com>
2019-03-26 05:59:37 +0000

    Automatically backed up by Learn

--- how_old_are_you.rb
+++ how_old_are_you.rb
@@ -1,7 +1,8 @@
 require_relative "./current_age_for_birth_year.rb"
 
 puts "What year were you born?"
+current_year = Time.now.year
 birth_year = gets.to_i
-users_age = current_age_for_birth_year(birth_year)
+users_age = current_year(birth_year)
 
 puts "You are #{users_age.to_s} years old."
