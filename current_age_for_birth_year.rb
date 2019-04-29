d46ceb7a4c6538fc31ab2c33151d35366c27b61b
Samuel Jean-Rosemond <js.rosemond@gmail.com>
2019-04-29 06:28:49 +0000

    Done.

---
+++ current_age_for_birth_year.rb
@@ -0,0 +1,8 @@
+def current_age_for_birth_year(birth_year)
+  puts birth_year
+  # current_year = 2019
+  year_used_in_test = 2003
+  persons_age = year_used_in_test - birth_year
+  puts persons_age
+  return persons_age
+end
