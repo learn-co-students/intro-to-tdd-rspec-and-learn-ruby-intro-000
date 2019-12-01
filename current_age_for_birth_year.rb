a6526485e832e8463ccff65707204fc126188b2f
Jorge Enrique Zafra <jorgezafra94@hotmail.com>
2019-12-01 23:02:09 +0000

    [feature] create a method that works as the test

---
+++ current_age_for_birth_year.rb
@@ -0,0 +1,8 @@
+# we are going to describe a method
+# in order to run our test suit sooooo
+# we have to have a method called current_age_for_birth_year(one_Arg)
+# an inside it we have to do something that when we put 1984 it returns me 19
+def current_age_for_birth_year(year_of_birth)
+  a = 2003 - year_of_birth
+  return (a)
+end
