e7385b687fcb5dbfc718d20973ca3cfbea62f2a1
Mykyta Melnyk <30383@pwsz-kalisz.edu.pl>
2020-05-20 09:36:48 +0000

    Done.

--- spec/current_age_for_birth_year_spec.rb
+++ spec/current_age_for_birth_year_spec.rb
@@ -1,9 +1,9 @@
 require_relative '../current_age_for_birth_year.rb'
 
 describe "current_age_for_birth_year method" do
-  it "returns the age of a person based on the year of birth" do
-    age_of_person = current_age_for_birth_year(1984)
+  it "should return age" do
+    age = current_age_for_birth_year(2003)
 
-    expect(age_of_person).to eq(19)
+    expect(age).to eq(17)
   end
 end
