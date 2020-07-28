0f938360c46069a96f624819b9f9df85f8f519bd
Mike Smith <gooseisdead@mac.com>
2020-07-28 15:54:24 +0000

    Done.

---
+++ how_old_are_you.rb
@@ -0,0 +1,11 @@
+require_relative './current_age_for_birth_year.rb'
+def current_age_for_birth_year(birth_year)
+  2020 - (birth_year)
+end
+
+puts "What year were you born?"
+birth_year = gets.to_i
+
+users_age = current_age_for_birth_year(birth_year)
+
+puts "You are: " + users_age.to_s + " years old."
